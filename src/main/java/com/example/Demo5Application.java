ab23b7cfa5b68e4d8f9f3ce03160fcfe4af60307
Mac <louis.niuniu@gmail.com>
2023-03-02 07:54:04 +0100

    add swagger ui

--- src/main/java/com/example/Demo5Application.java
+++ src/main/java/com/example/Demo5Application.java
@@ -1,5 +1,6 @@
 package com.example;
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
 
 @EnableWebMvc
 @SpringBootApplication
